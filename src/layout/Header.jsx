cb4dc05ab676a891018450e6ae7e5b914e4153e1
Evgeniy Shpilco <callelia@yandex.ru>
2025-06-04 22:58:02 +0600

    project complete

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
-    return <nav className="blue darken-1">
+    return <nav className="blue darken-3">
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React Movies</a>
+            <a href="!#" className="brand-logo">React Movies</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!s#">Repo</a></li>
+                <li><a href="!#">Repo</a></li>
             </ul>
         </div>
     </nav>
